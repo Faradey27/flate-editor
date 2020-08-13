72e6b350acc0bac69dbece52655bd1fcdcea24e6
Andrii Tiertyshnyi <andrii.tiertyshnyi@gmail.com>
2020-08-13 19:08:12 +0300

    Fix colors parsing in production

--- src/services/editor/plugins/editorTheme.ts
+++ src/services/editor/plugins/editorTheme.ts
@@ -1,12 +1,12 @@
 const toHexNumberColor = (color: string) => {
-  return Number(`0x${color.substr(2, color.length)}`);
+  return Number(`0x${color.slice(1)}`);
 };
 
 export const getEditorTheme = () => {
   const styles = global.getComputedStyle(document.documentElement);
 
-  const primaryColor = styles.getPropertyValue('--primaryColor');
-  const backgroundColor = styles.getPropertyValue('--backgroundColor');
+  const primaryColor = styles.getPropertyValue('--primaryColor').trim();
+  const backgroundColor = styles.getPropertyValue('--backgroundColor').trim();
 
   return {
     primaryColor: toHexNumberColor(primaryColor),
